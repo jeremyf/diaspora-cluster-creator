d14b536548c626760dfd4acdeee80a9deb8fa88f
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2012-04-10 19:22:27 -0400

    Moved with_loaded_dice method to be a helper.

--- spec/spec_helper_lite.rb
+++ spec/spec_helper_lite.rb
@@ -3,6 +3,18 @@ require 'minitest/spec'
 require 'minitest/autorun'
 require 'ostruct'
 $: << File.expand_path('../lib/diaspora-cluster-creator', File.dirname(__FILE__))
+require 'diaspora-cluster-creator'
 
 # Cheating a bit to help tests be a bit more readable
 include Diaspora::Cluster::Creator
+
+class MiniTest::Unit::TestCase
+
+  def with_loaded_dice(roll, object = subject)
+    loaded_dice = MiniTest::Mock.new
+    loaded_dice.expect(:roll, roll)
+    object.dice = loaded_dice
+    yield
+    loaded_dice.verify
+  end
+end
