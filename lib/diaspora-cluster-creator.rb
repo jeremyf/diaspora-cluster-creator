e7f78e6120382766b810c4ced020ee0cf19d8c0e
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2012-04-12 17:38:04 -0400

    Changed command line to default write to STDOUT

--- lib/diaspora-cluster-creator.rb
+++ lib/diaspora-cluster-creator.rb
@@ -4,13 +4,16 @@ module Diaspora
   module Cluster
     module Creator
       def self.run(params)
+        filename = params[:filename].to_s
         names = ('A'..'Z').to_a[0,params[:count] || 6]
         cluster = Cluster.new( params[:names] || names )
         graph = Graph.new(cluster)
         template = Template.new(graph)
-        case File.extname(params[:filename])
+        case File.extname(filename)
         when '.dot' then template.to_dot(params[:filename])
         when '.png' then template.to_png(params[:filename])
+        else
+          STDOUT.write template.to_s
         end
       end
     end
